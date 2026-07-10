a4f31b4a93750fe0fcfdee4b20b6b186bf87875d
ShatrughnPinjari <tusharpinjari24@gmail.com>
2026-07-10 11:40:36 +0530

    Add first Demo Springboot Application

---
+++ demo/src/main/java/com/firstSpringbootApplication/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.firstSpringbootApplication.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoApplication.class, args);
+
+//		System.out.println("Hello World!");
+//		HelloController hello = new HelloController();
+//		String s = hello.sayHello();
+//		System.out.println(s);
+	}
+
+}
